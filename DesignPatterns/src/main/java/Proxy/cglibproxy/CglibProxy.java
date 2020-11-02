ec39d8b85dbfbec1307b7113f213c7437a0dcdbb
yangshu <yang_shu@muorngtech.com>
2020-11-02 16:58:45 +0800

    设计模式

--- DesignPatterns/src/main/java/Proxy/cglibproxy/CglibProxy.java
+++ DesignPatterns/src/main/java/Proxy/cglibproxy/CglibProxy.java
@@ -1,48 +1,48 @@
-package Proxy.cglibproxy;
-
-import net.sf.cglib.proxy.Enhancer;
-import net.sf.cglib.proxy.MethodInterceptor;
-import net.sf.cglib.proxy.MethodProxy;
-
-import java.lang.reflect.Method;
-
-/**
- * Created by Tom on 2019/3/11.
- */
-public class CglibProxy implements MethodInterceptor {
-
-
-    public Object getInstance(Class<?> clazz) throws Exception{
-        //相当于Proxy，代理的工具类
-        //相当于Proxy.newInstance
-        Enhancer enhancer = new Enhancer();
-        enhancer.setSuperclass(clazz);
-        //MethodInterceptor继承了callback
-        enhancer.setCallback(this);
-        return enhancer.create();
-    }
-
-    /**
-     *
-     * @param o
-     * @param method
-     * @param objects
-     * @param methodProxy
-     */
-    @Override
-    public Object intercept(Object o, Method method, Object[] objects, MethodProxy methodProxy) throws Throwable {
-        before();
-        Object obj = methodProxy.invokeSuper(o,objects);
-        after();
-        return obj;
-    }
-
-    private void before(){
-        System.out.println("我是媒婆，我要给你找对象，现在已经确认你的需求");
-        System.out.println("开始物色");
-    }
-
-    private void after(){
-        System.out.println("OK的话，准备办事");
-    }
-}
+//package Proxy.cglibproxy;
+//
+//import net.sf.cglib.proxy.Enhancer;
+//import net.sf.cglib.proxy.MethodInterceptor;
+//import net.sf.cglib.proxy.MethodProxy;
+//
+//import java.lang.reflect.Method;
+//
+///**
+// * Created by Tom on 2019/3/11.
+// */
+//public class CglibProxy implements MethodInterceptor {
+//
+//
+//    public Object getInstance(Class<?> clazz) throws Exception{
+//        //相当于Proxy，代理的工具类
+//        //相当于Proxy.newInstance
+//        Enhancer enhancer = new Enhancer();
+//        enhancer.setSuperclass(clazz);
+//        //MethodInterceptor继承了callback
+//        enhancer.setCallback(this);
+//        return enhancer.create();
+//    }
+//
+//    /**
+//     *
+//     * @param o
+//     * @param method
+//     * @param objects
+//     * @param methodProxy
+//     */
+//    @Override
+//    public Object intercept(Object o, Method method, Object[] objects, MethodProxy methodProxy) throws Throwable {
+//        before();
+//        Object obj = methodProxy.invokeSuper(o,objects);
+//        after();
+//        return obj;
+//    }
+//
+//    private void before(){
+//        System.out.println("我是媒婆，我要给你找对象，现在已经确认你的需求");
+//        System.out.println("开始物色");
+//    }
+//
+//    private void after(){
+//        System.out.println("OK的话，准备办事");
+//    }
+//}
