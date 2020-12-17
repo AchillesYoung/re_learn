60d76abb44be6dc28c82f4eeca620b8f87827682
yangshu <yang_shu@muorngtech.com>
2020-12-17 16:04:38 +0800

    算法

--- Algorithm/src/Lead2Offer/sort/HeapSort.java
+++ Algorithm/src/Lead2Offer/sort/HeapSort.java
@@ -6,20 +6,23 @@ public class HeapSort {
      * 构建大顶堆，然后后数组最后一位换位置
      */
     public int[] heap_sort(int[] unsorted_arry) {
-
-        if (unsorted_arry == null || unsorted_arry.length < 2) {
+        int size = unsorted_arry.length;
+        if (unsorted_arry == null || size < 2) {
             return unsorted_arry;
         }
+
         //step1：先把原始数组构建第一次大顶堆，后续再交换重做
         //循环遍历数组，把元素依次插入构造的大顶堆，向下构造
-        for (int i = 0; i < unsorted_arry.length; ++i) {
+        for (int i = 0; i < size; ++i) {
             heap_insert(unsorted_arry, i);
         }
         //step2：现在数组的头节点是最大，swap一下，数组变小再次构建，这次不是insert了，因为只是堆头变了
-        swap(unsorted_arry, 0, unsorted_arry.length - 1);
-        for (int i = unsorted_arry.length - 1; i > 0; --i) {
-            heapify(unsorted_arry, 0, i);
-            swap(unsorted_arry, 0, i);
+        swap(unsorted_arry, 0, --size);
+        while (size > 0) {
+            //这里传的是size，不是end，索引要size-1
+            heapify(unsorted_arry, 0, size);
+            //先赋值再--
+            swap(unsorted_arry, 0, --size);
         }
         return unsorted_arry;
     }
@@ -59,20 +62,23 @@ public class HeapSort {
          * 先拿左节点（子树节点计算index * 2 + 1）
          */
         int left = index * 2 + 1;
-        while (left < size) {
+        //这里判断子节点索引 left <= size -1
+        while (left <= size - 1) {
+            //两次比较拿到三个节点最大值索引
+            int largest = left + 1 <= size - 1 && arr[left + 1] >arr[left] ? left+1 : left;
             //先对比二个子节点，拿到最大的节点的索引，再去和父节点index比较，最终拿到largest
-            int largest = left + 1 < size && arr[left + 1] > arr[left] ? left + 1 : left;
             //这里拿到了子树里面最大的元素索引
             largest = arr[largest] > arr[index] ? largest : index;
             //如果最大的就是index，结束循环，不然交换继续向下递归
             if (largest == index) {
                 break;
             }
-//            交换，传递位置
+            //这里子节点大了，要交换父节点，向下传递
             swap(arr, largest, index);
-            //继续向下，stack递归遍历（
+            //继续向下，stack递归遍
             // 这里没有左右子树的概念是因为哪个节点出来了换位就基于哪个节点继续向下遍历。直到叶节点
             index = largest;
+            //算下更新left去判断是否下次会越界
             left = index * 2 + 1;
         }
 
